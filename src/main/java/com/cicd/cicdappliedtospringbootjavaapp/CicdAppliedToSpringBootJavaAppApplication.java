caa73b1d83196be76570daafae777fa17d9700da
gshah188 <gshah188@yahoo.com>
2020-02-04 12:30:37 +0500

    Updated POm and Added Home controller and updated Unit test class

--- src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
+++ src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
@@ -2,18 +2,10 @@ package com.cicd.cicdappliedtospringbootjavaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class CicdAppliedToSpringBootJavaAppApplication {
-	
-	@RequestMapping("/")
-	public String home() {
-		return " Hello World";
-	}
 
+public class CicdAppliedToSpringBootJavaAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CicdAppliedToSpringBootJavaAppApplication.class, args);
 	}
